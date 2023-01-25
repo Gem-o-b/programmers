0c08b123df7cd3d8cb072c044ce42c1cfd49681e
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-01-25 22:55:42 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -2,15 +2,13 @@ class Solution {
     public int solution(int n) {
        int answer = 0;
         int total =1;
-        while(n != total){
+        while(true){
             if (total*(answer+1) > n){
                 break;
             }
             total *=++answer;
         }
-        if (answer==0){
-            answer=1;
-        }
+       
         return answer;
     }
 }
