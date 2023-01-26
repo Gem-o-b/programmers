a92861e97fe056fa68e41b63912e0a28a1e6fe13
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-01-27 00:13:54 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.15 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,8 @@
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int n) {
+         return Arrays.stream(String.valueOf(n).split("")).mapToInt(i->Integer.parseInt(i)).sum();
+    }
+}
