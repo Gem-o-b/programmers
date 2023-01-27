ac409d04223ee2b3ff248369d1cf95e5e81fb538
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-01-27 20:07:13 +0900

    [level 1] Title: 하샤드 수, Time: 1.73 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public boolean solution(int x) {
-        return x%Arrays.stream(String.valueOf(x).split("")).mapToInt(Integer::parseInt).reduce(0,(a,b)->a+b)==0?true:false;
+        return x%Arrays.stream(String.valueOf(x).split("")).mapToInt(Integer::parseInt).reduce(0,(a,b)->a+b)==0;
     }
 }
