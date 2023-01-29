a707be5debc91acdf709e68773817edc80d8d2b8
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-01-29 22:30:25 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] solution(int[] array) {
-       int[] answer = {0,0};
         int tmp = 0;
         for (int i = 0; i < array.length; i++) {
-            
             if (array[tmp] < array[i]){
                 tmp = i;
-
             }
-
         }
-        answer[0] = array[tmp];
-        answer[1] = tmp;
+        int[] answer = {array[tmp],tmp};
         return answer;
     }
 }
