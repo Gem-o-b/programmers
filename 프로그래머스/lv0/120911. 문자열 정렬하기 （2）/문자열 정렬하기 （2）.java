8f765a2f201e7d29049b02c39a59a9546dd4952f
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-01-31 22:23:40 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.72 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+class Solution {
+    public String solution(String my_string) {
+         return Arrays.stream(my_string.toLowerCase().split("")).sorted().collect(Collectors.joining());
+    }
+}
