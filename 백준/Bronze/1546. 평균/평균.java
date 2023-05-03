5fbe11109fd98c0fef9e38876126add0c07e8a6b
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-05-03 22:54:31 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,18 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
 import java.util.Arrays;
 
-
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		double arr[] = new double [Integer.parseInt(br.readLine())];
-		String tmp[] = br.readLine().split(" ");
-		for(int i =0;i<arr.length ; i++) {
-			arr[i] = Integer.parseInt(tmp[i]);
-		}
-		Arrays.sort(arr);
-		double arrtmp = Arrays.stream(arr).map(i->i/arr[arr.length-1]*100).sum();
-		arrtmp = arrtmp/arr.length;
-		System.out.println(arrtmp);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long cnt = Long.parseLong(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split( " ")).mapToInt(Integer::parseInt).toArray();
+        int max = 0;
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) max = arr[i];
+            total += arr[i];
+        }
+        System.out.println((double)total* 100L/ max /cnt);
+    }
 }
