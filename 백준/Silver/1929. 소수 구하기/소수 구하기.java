2bd5079e1626df18a46f4f48ea23592e4dcc3cb8
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-05-05 14:37:30 +0900

    [Silver III] Title: 소수 구하기, Time: 1084 ms, Memory: 35388 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int[] arr = new int[M+1];
+
+        for (int i = 2; i <= M; i++) {
+            arr[i] = i ;
+        }
+
+        for (int i = 2; i <= Math.sqrt(M); i++) {
+            if(arr[i] ==0) continue;
+            for (int j = i+i; j <= M ; j+=i) {
+                arr[j] = 0;
+            }
+        }
+
+        for (int i = N; i <= M; i++) {
+            if (arr[i] ==0)continue;
+            System.out.println(arr[i]);
+        }
+    }
+
+}
