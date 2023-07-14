0a61ee5664e847b67096cdd0c37e38cbd1ebdebb
Gem-o-b <111578825+Gem-o-b@users.noreply.github.com>
2023-07-14 13:19:58 +0900

    [Bronze V] Title: 문자열, Time: 140 ms, Memory: 14932 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String text = br.readLine();
+            System.out.println(text.charAt(0)+""+text.charAt(text.length()-1));
+        }
+    }
+}
